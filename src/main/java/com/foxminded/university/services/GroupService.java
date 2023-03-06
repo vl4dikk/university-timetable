c2da1495e555de3792cf9cba2d0545d38d02dc45
vladr <vladr@host.docker.internal>
2023-03-06 18:08:11 +0200

    added group service and tests with mockito

---
+++ src/main/java/com/foxminded/university/services/GroupService.java
@@ -0,0 +1,48 @@
+package com.foxminded.university.services;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.foxminded.university.dao.GroupDao;
+import com.foxminded.university.models.Group;
+
+@Service
+public class GroupService {
+	private GroupDao dao;
+
+	@Autowired
+	public GroupService(GroupDao dao) {
+		this.dao = dao;
+	}
+
+	public void insert(String name) {
+		Group group = new Group();
+		group.setName(name);
+		dao.insert(group);
+	}
+	
+	public void insert(List<String> names) {
+		List<Group> groups = new LinkedList<>();
+		for (String name : names) {
+			Group group = new Group();
+			group.setName(name);
+			groups.add(group);
+		}
+		dao.insert(groups);
+	}
+	
+	public void deleteById(int groupId) {
+		dao.deleteById(groupId);
+	}
+	
+	public List<Group> getAllGroups() {
+		return dao.getAllGroups();
+	}
+	
+	public Group getGroupById(int groupId) {
+		return dao.getGroupById(groupId);
+	}
+}
---
+++ src/test/java/com/foxminded/university/services/GroupServiceTest.java
@@ -0,0 +1,61 @@
+package com.foxminded.university.services;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.foxminded.university.dao.GroupDao;
+import com.foxminded.university.models.Group;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class GroupServiceTest {
+
+	@Mock
+	GroupDao dao;
+
+	@InjectMocks
+	GroupService service;
+
+	@Test
+	void testInsert() {
+		service.insert(anyString());
+		verify(dao, times(1)).insert(any(Group.class));
+	}
+
+	@Test
+	void testInsertListOfString() {
+		service.insert(anyList());
+		verify(dao, times(1)).insert(anyList());
+	}
+
+	@Test
+	void testDeleteById() {
+		service.deleteById(anyInt());
+		verify(dao, times(1)).deleteById(anyInt());
+	}
+
+	@Test
+	void testGetAllGroups() {
+		service.getAllGroups();
+		verify(dao, times(1)).getAllGroups();
+	}
+
+	@Test
+	void testGetGroupById() {
+		service.getGroupById(anyInt());
+		verify(dao, times(1)).getGroupById(anyInt());
+	}
+
+}
