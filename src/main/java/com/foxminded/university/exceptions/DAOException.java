e33d41705bc38df469377f8158681800c81007b7
vladr <vladr@host.docker.internal>
2023-03-21 15:16:28 +0200

    added custom dao exception

---
+++ src/main/java/com/foxminded/university/exceptions/DAOException.java
@@ -0,0 +1,11 @@
+package com.foxminded.university.exceptions;
+
+public class DAOException extends RuntimeException {
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
